af990596e6bb76039df471e65ceaba3895c2812f
Arvis Abromovics <info@vsmedio.lv>
2018-01-07 23:24:14 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,41 @@
+/*
+Переставь один модификатор static
+Переставь один модификатор static, чтобы пример скомпилировался.
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. В классе должна быть переменная A.
+3. В классе должна быть переменная B.
+4. В классе должна быть переменная C.
+5. В классе должен быть метод main.
+6. В классе должен быть метод getValue.
+7. В классе должно быть 4 модификатора static (переменные и методы).
+
+
+public class Solution {
+    public final int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void main(String[] args) {
+    }
+
+    public static int getValue() {
+        return C;
+    }
+}
+*/
+
+public class Solution {
+    public static final int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+
+    public static void main(String[] args) {
+    }
+
+    public  int getValue() {
+        return C;
+    }
+}
